d27e15c63a18c4c241f954a7bb45be9ded5c57e7
vichetmeng <mengvichet@hotmail.com>
2019-10-28 14:16:45 -0700

    Modified tablescript and added User entity

---
+++ src/main/java/com/redditclone/entity/UserEntity.java
@@ -0,0 +1,50 @@
+package com.redditclone.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+	@Id
+	private Integer uid;
+	private String email;
+	private String passwordHash;
+	private String username;
+	@Column(name = "avatar")
+	private String avatarUrl;
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
+	}
+	
+	
+}
