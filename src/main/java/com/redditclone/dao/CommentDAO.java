5d4268c743a2b563adfcbd51e7324362b2ad3860
Khang Nguyen <nkhang2112@yahoo.com.vn>
2019-11-04 14:40:11 -0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/vichetmeng/Reddit_Clone_Server
    
    # Conflicts:
    #	src/main/java/com/redditclone/dao/UserDAOImpl.java

--- src/main/java/com/redditclone/dao/CommentDAO.java
+++ src/main/java/com/redditclone/dao/CommentDAO.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.redditclone.dao;
 
 import java.util.List;
@@ -11,17 +11,3 @@ public interface CommentDAO {
 	public Integer createComment(Integer pid, Comment comment, Integer uid, Integer parentid) throws Exception;
 	public Boolean deleteComment(Integer cid) throws Exception;
 }
-=======
-package com.redditclone.dao;
-
-import java.util.List;
-
-import com.redditclone.model.Comment;
-
-public interface CommentDAO {
-	public List<Comment> getAllComment(Integer pid);
-	public Comment updateComment(Comment comment);
-	public Integer createComment(Integer pid, Comment comment, Integer uid, Integer parentid);
-	public Boolean deleteComment(Integer cid);
-}
->>>>>>> 325313ccd2c7f7332b2b6b05bf3ddff2eed79922
--- src/main/java/com/redditclone/dao/PostDAO.java
+++ src/main/java/com/redditclone/dao/PostDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.redditclone.dao;
 
 import com.redditclone.model.Post;
@@ -8,14 +7,3 @@ public interface PostDAO {
 	public Boolean updatePost (Post post) throws Exception;
 	public Boolean deletePost(Integer pid) throws Exception;  
 }
-=======
-package com.redditclone.dao;
-
-import com.redditclone.model.Post;
-
-public interface PostDAO {
-	public Integer createPost (Integer uid, Integer tid, Post post);
-	public Post updatePost (Post post);
-	public Boolean deletePost(Integer pid);  
-}
->>>>>>> 325313ccd2c7f7332b2b6b05bf3ddff2eed79922
